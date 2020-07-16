349f53c6728a46584de43495a2eb3e95af45cd62
ValiVKhan <31572013+ValiVKhan@users.noreply.github.com>
2020-07-16 12:30:35 -0500

    First commit

---
+++ IP_addy.py
@@ -0,0 +1,19 @@
+import requests 
+import get
+
+proxies={
+    'https':'77.120.152.161:33265',
+    'https':'77.120.152.161:33265'
+}
+
+url = 'https://httpbin.org/ip'
+resp = requests.get(url,proxies = proxies)
+print(resp.json)
+print(resp.text)
+
+#url = 'https://httpbin.org/ip'
+
+#resp = requests.get(url)
+
+#print(resp.json)
+#print (resp.text)
